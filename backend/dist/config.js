db34eb2166ed3cfbc23d8476fdd373abf73d6f6f
Nicholas Guarino <sub4miler5@gmail.com>
2023-08-25 00:08:23 -0400

    Delete backend/dist directory

--- backend/dist/config.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-/** Shared config for application; can be required many places. */
-Object.defineProperty(exports, "__esModule", { value: true });
-const PORT = process.env.PORT || 3001;
-exports.default = PORT;
