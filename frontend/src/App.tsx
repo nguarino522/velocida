57d6082b13d9dc43b4282431197eb3d564c32b95
Josh Guarino <jguarino722@gmail.com>
2023-09-04 16:59:20 -0400

    react router basic setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import Nav from "./components/Nav"
+import Routing from "./routes/routing"
 
 export default function App() {
   return (
     <>
       <Nav></Nav>
+      <Routing></Routing>
     </>
   )
 }
