34f9d7767bac10951e2a3fcd7d68f70765aa4e0d
Josh Guarino <jguarino722@gmail.com>
2023-09-09 15:58:53 -0400

    refactor models/user.ts

---
+++ backend/src/models/activity.ts
@@ -0,0 +1,14 @@
+import { Activity } from "@prisma/client";
+
+export default class Activities {
+    static async createActivity(activity: Activity) {
+
+    } 
+
+    static async getActivity(activityId: number) {
+
+    }
+}
+
+
+
--- backend/src/models/user.ts
+++ backend/src/models/user.ts
@@ -1,65 +1,91 @@
 import prisma from "../prisma"
 import { User } from "@prisma/client"
 import { BadRequestError, NotFoundError } from "../expressError"
+import { promises } from "dns"
 
 interface registerUser {
     email: string,
     username: string,
 }
 
+interface removeUser {
+    email: string,
+    username: string|null,
+}
+
 export default class Users {
 
-    static async register(requestBody: registerUser) {
+    /**
+     * check for duplicate fields on register new user
+     * @param username 
+     * @param email 
+     * @throws {BadRequestError}
+     */
+    private static async checkDuplicates(username: string, email: string): Promise<void> {
+        if (await prisma.user.findUnique({ where: { username: username } })) {
+            throw new BadRequestError(`Duplicate username Found: ${username}`)
+        }
+        if (await prisma.user.findUnique({ where: { email: email } })) {
+            throw new BadRequestError(`Duplicate email Found: ${email}`)
+        }
+    }
+
+    /**
+     * register new user
+     * @param requestBody {registerUser}
+     * @returns {Promise<User>}
+     */
+    static async register(requestBody: registerUser): Promise<User> {
         const { email, username } = requestBody
-        
-        // throw error if username already taken / in use
-        const duplicateUsername = await prisma.user.findUnique({
-            where: { username: username }
-        })
-        if (duplicateUsername) {
-            throw new BadRequestError(`Duplicate Username Found: ${username}`)
-        }
+        this.checkDuplicates(username, email)
 
-        // throw error if email already taken / in use
-        const duplicateEmail = await prisma.user.findUnique({
-            where: { email: email }
-        })
-        if (duplicateEmail) {
-            throw new BadRequestError(`Duplicate Email Found: ${email}`)
-        }
-
-        // attempt to create the new user
         const user = await prisma.user.create({
             data: {
                 email: email,
                 username: username
             }
         })
+
         return user
     }
 
-
-    static async getAll() {
+    /**
+     * get all registered users
+     * @returns {Promise<User[]>}
+     */
+    static async getAll(): Promise<User[]> {
         const users = await prisma.user.findMany()
+
         return users
     }
 
-
-    static async get(username: string) {
+    /**
+     * get user by username
+     * @param username 
+     * @returns {Promise<User>}
+     * @throws {NotFoundError}
+     */
+    static async get(username: string): Promise<User> {
         const user = await prisma.user.findUnique({
             where: { username: username }
         })
         if (!user) throw new NotFoundError(`User Not Found: ${username}`);
+
         return user
     }
 
-
-    static async remove(username: string) {
+    /**
+     * delete user by username
+     * @param username 
+     * @returns {Promise<removeUser>}
+     * @throws {NotFoundError}
+     */
+    static async remove(username: string): Promise<removeUser> {
         const user = await prisma.user.delete({
             where: { username: username },
             select: { email: true, username: true }
         }).catch(() => { throw new NotFoundError(`User Not Found: ${username}`) })
-        // if (!user) throw new NotFoundError(`User Not Found: ${username}`)
+
         return user
     }
 
