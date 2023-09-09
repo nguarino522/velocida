aff1cec5a00a36bbc514363858d9bc32f0882d84
Josh Guarino <jguarino722@gmail.com>
2023-09-09 17:03:41 -0400

    added models/activity.ts

--- backend/src/models/activity.ts
+++ backend/src/models/activity.ts
@@ -1,16 +1,48 @@
 import { Activity } from "@prisma/client";
+import prisma from "../prisma";
+import { BadRequestError, NotFoundError } from "../expressError"
 
 interface createActivity {
-    
+    title: string,
+    description: string,
+    duration: number,
+    distance: number,
 }
 
 export default class Activities {
-    static async createActivity(activity: Activity) {
+    /**
+     * create an activity 
+     * @param requestBody 
+     * @returns {Promise<Activity>}
+     */
+    static async create(requestBody: createActivity): Promise<Activity> {
+        const {title, description, duration, distance} = requestBody
+        const activity = await prisma.activity.create({
+            data: {
+                title: title,
+                description: description,
+                duration: duration,
+                distance: distance,
+                ownerId: 1 // todo: need userId from logged in user, might fail 
+            }
+        })
 
+        return activity
     } 
 
-    static async getActivity(activityId: number) {
-
+    /**
+     * get activity by id
+     * @param activityId 
+     * @returns {Promise<Activity>}
+     * @throws {NotFoundError}
+     */
+    static async get(activityId: number): Promise<Activity> {
+        const activity = await prisma.activity.findUnique({
+            where: { id: activityId }
+        })
+        if (!activity) throw new NotFoundError(`Activity Not Found: ${activityId}`);
+    
+        return activity
     }
 }
 
