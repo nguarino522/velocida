ac29d67bf80b0463e0036f8805dee8eae4630761
nguarino522 <sub4miler5@gmail.com>
2023-11-06 21:48:37 -0500

    navbar and routing fix

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import { Navbar, Nav } from "react-bootstrap";
-import { NavLink } from "react-router-dom"
+import { NavLink, Link } from "react-router-dom"
 import UserContext from "../UserContext";
 import "./NavBar.css"
 import img from "../assets/runner.png"
@@ -12,11 +12,11 @@ const NavBar = ({ logout }) => {
     return (
       <Navbar className="NavBar" >
         <Navbar.Brand className="m-3" href="/"><img src={img} />Velocida</Navbar.Brand>
-        <Nav variant="pills" className="ms-right p-3" activeKey={window.location.pathname}>
-          <Nav.Link className="nav-link" href="/">Home</Nav.Link>
-          <Nav.Link className="nav-link" href="/dashboard">Dashboard</Nav.Link>
-          <Nav.Link className="nav-link" href="/news">News</Nav.Link>
-          <Nav.Link className="nav-link" href="/forum">Forum</Nav.Link>
+        <Nav variant="pills" className="ms-right p-3">
+          <NavLink className="nav-link" to="/">Home</NavLink>
+          <NavLink className="nav-link" to="/dashboard">Dashboard</NavLink>
+          <NavLink className="nav-link" to="/news">News</NavLink>
+          <NavLink className="nav-link" to="/forum">Forum</NavLink>
         </Nav>
         <Nav variant="pills" className="ms-auto p-3">
           <Nav.Link href="/profile">Profile</Nav.Link>
@@ -30,14 +30,14 @@ const NavBar = ({ logout }) => {
     return (
       <Navbar className="NavBar">
         <Navbar.Brand className="m-3" href="/"><img src={img} />Velocida</Navbar.Brand>
-        <Nav variant="pills" className="ms-right p-3" activeKey={window.location.pathname}>
-          <Nav.Link className="nav-link" href="/">Home</Nav.Link>
-          <Nav.Link href="/news">News</Nav.Link>
-          <Nav.Link className="nav-link" href="/forum">Forum</Nav.Link>
+        <Nav variant="pills" className="ms-right p-3">
+          <NavLink className="nav-link" to="/">Home</NavLink>
+          <NavLink className="nav-link" to="/news">News</NavLink>
+          <NavLink className="nav-link" to="/forum">Forum</NavLink>
         </Nav>
         <Nav variant="pills" className="ms-auto p-3" activeKey={window.location.pathname}>
-          <Nav.Link href="/login">Login</Nav.Link>
-          <Nav.Link href="/signup">SignUp</Nav.Link>
+          <NavLink className="nav-link" to="/login">Login</NavLink>
+          <NavLink className="nav-link" to="/signup">SignUp</NavLink>
         </Nav>
       </Navbar>
     )
