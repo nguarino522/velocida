4de6144546d19c3a739969ac43b0d0e5cef71033
nguarino522 <sub4miler5@gmail.com>
2024-01-28 15:35:57 -0500

    updoots

--- frontend/src/VelocidaApi.js
+++ frontend/src/VelocidaApi.js
@@ -4,7 +4,7 @@ import axios from "axios";
 // const BASE_URL = import.meta.env.REACT_APP_BASE_URL || "http://localhost:5227";
 
 function getBaseUrl() {
-  return import.meta.env.REACT_APP_BASE_URL || "http://localhost:5227"
+  return process.env.REACT_APP_BASE_URL || "http://localhost:5227";
 }
 
 /** API Class.
