4a9e525fd904a0beb615dc7e2c848a5f4aa25b5a
nguarino522 <sub4miler5@gmail.com>
2024-01-27 22:39:36 -0500

    cleanup

--- frontend/src/VelocidaApi.js
+++ frontend/src/VelocidaApi.js
@@ -43,29 +43,6 @@ class VelocidaApi {
     return res
   }
 
-  /** Get details on a company by handle. */
-  static async getCompany(handle) {
-    let res = await this.request(`companies/${handle}`);
-    return res.company;
-  }
-
-  /** Get companies (filtered by name if not undefined) */
-  static async getCompanies(name) {
-    let res = await this.request("companies", { name });
-    return res.companies;
-  }
-
-  /** Get list of jobs (filtered by title if not undefined) */
-  static async getJobs(title) {
-    let res = await this.request("jobs", { title });
-    return res.jobs;
-  }
-
-  /** Apply to a job */
-  static async applyToJob(username, id) {
-    await this.request(`users/${username}/jobs/${id}`, {}, "post");
-  }
-
   /** Get token for login from username, password. */
   static async login(data) {
     let res = await this.request(`auth/token`, data, "post");
